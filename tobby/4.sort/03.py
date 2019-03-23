3d4e330b30d2463018714bbc68bbdddb5e7a92a6
jjinmo <wlsah7505@korea.ac.kr>
2019-03-23 16:31:34 +0900

    코드수정

--- tobby/4.sort/03.py
+++ tobby/4.sort/03.py
@@ -1,6 +1,6 @@
 import numpy as np
 def solution(citations):
     c = np.array(citations)
-    for h in range(max(c)):
+    for h in range(max(c), -1, -1):
         if h <= np.sum(c>=h):
             return h
