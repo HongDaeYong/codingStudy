034093ae64452defe8fc2c76a37f7c377f0ae3d4
hongdaeyong <ejddjfl67@gmail.com>
2019-03-26 22:29:39 +0900

    ㅜㅜ

--- DaeYong/1hash/hashProb4.py
+++ DaeYong/1hash/hashProb4.py
@@ -1,33 +1,16 @@
-class song:
-    def __init__(self, genres):
-        self.genres = genres
-        self.plays = []
-        self.total = 0
-
-    def appendSong(self, number, play):
-        self.plays.append([number, play])
-        self.total += play
-
-
 def solution(genres, plays):
     answer = []
-    songs = {}
-    for i, (g, p) in enumerate(zip(genres, plays)):
-        if g not in songs:
-            songs[g] = song(g)
-        songs[g].appendSong(i, p)
+    twoLVhash = {}
+    for id, (gen, pl) in enumerate(zip(genres, plays)):
+        if gen not in twoLVhash:
+            twoLVhash[gen] = {}
+        twoLVhash[gen][id] = pl
 
-    genresSort = []
-    for s in songs:
-        songs[s].plays.sort(key=lambda x: x[1], reverse=True)
-        genresSort.append([songs[s].genres, songs[s].total])
-    genresSort.sort(key=lambda x: x[1], reverse=True)
-    print(genresSort)
-
-    for gsort in genresSort:
-        answer.append(songs[gsort[0]].plays[0][0])
-        answer.append(songs[gsort[0]].plays[1][0])
-
+    srtd = sorted(twoLVhash.items(), key=lambda x: sum(x[1].values()), reverse=True)
+    for sd in srtd:
+        tmpSd = sorted(sd[1].items(), key=lambda x: x[1], reverse=True)
+        answer.append(tmpSd[0][0])
+        answer.append(tmpSd[1][0])
     return answer
 
 
