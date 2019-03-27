a221d9a42ae708b5cc463964d1578a6d3578dc95
hongdaeyong <ejddjfl67@gmail.com>
2019-03-27 17:57:21 +0900

    오예 해시 4번 푸러따

--- DaeYong/1hash/hashProb4.py
+++ DaeYong/1hash/hashProb4.py
@@ -10,7 +10,8 @@ def solution(genres, plays):
     for sd in srtd:
         tmpSd = sorted(sd[1].items(), key=lambda x: x[1], reverse=True)
         answer.append(tmpSd[0][0])
-        answer.append(tmpSd[1][0])
+        if len(tmpSd) > 1:
+            answer.append(tmpSd[1][0])
     return answer
 
 
