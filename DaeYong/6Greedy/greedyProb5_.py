51c1a2f94f86a888e012f1c02098b441ea632759
hongdaeyong <ejddjfl67@gmail.com>
2019-04-20 13:50:02 +0900

    그리디 5번

--- DaeYong/6Greedy/greedyProb5_.py
+++ DaeYong/6Greedy/greedyProb5_.py
@@ -1,6 +1,16 @@
 def solution(n, costs):
     answer = 0
-    
+    tmpAns = 0
+    connected = []
+    costs.sort(key=lambda x: x[2])
+    for cost in costs:
+        if cost[0] not in connected or cost[1] not in connected:
+            answer += cost[2]
+            tmpAns += cost[2]
+            if cost[0] not in connected:
+                connected.append(cost[0])
+            if cost[1] not in connected:
+                connected.append(cost[1])
     return answer
 
 
