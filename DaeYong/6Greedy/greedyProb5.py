4546be214a4c1b2ec2c735126b22f95796b714df
hongdaeyong <ejddjfl67@gmail.com>
2019-04-21 17:39:42 +0900

    섬 연결하기 푸러따 오예 그리디 다품

--- DaeYong/6Greedy/greedyProb5.py
+++ DaeYong/6Greedy/greedyProb5.py
@@ -17,8 +17,19 @@ def solution(n, costs):
     answer = 0
     connected = [0]
     for i in range(n-1):
-        pass
-
+        min = 9999999999
+        minNode = -1
+        for cost in costs:
+            if cost[0] in connected and cost[1] not in connected:
+                if min > cost[2]:
+                    min = cost[2]
+                    minNode = cost[1]
+            elif cost[0] not in connected and cost[1] in connected:
+                if min > cost[2]:
+                    min = cost[2]
+                    minNode = cost[0]
+        connected.append(minNode)
+        answer += min
     return answer
 
 
