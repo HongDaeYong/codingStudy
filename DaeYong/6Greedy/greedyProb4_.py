54258a02ab1057de649e3763bad2a888e3db34c7
hongdaeyong <ejddjfl67@gmail.com>
2019-05-11 13:39:45 +0900

    단속카메라 풂. // 구명보트 50점으로 개선

--- DaeYong/6Greedy/greedyProb4_.py
+++ DaeYong/6Greedy/greedyProb4_.py
@@ -1,34 +1,34 @@
-# 40 점  // 최대 두명만 탈수 있음!! 그 조건 빼먹고 풂.
-def solution(people, limit):
-    minimum = 40
-    rescued = [1 if limit - minimum < person else 0 for person in people]
-    answer = sum(rescued)
-    while sum(rescued) < len(people):
-        boat = 0
-        onBoard = {}
-        for i, (mask, person) in enumerate(zip(rescued, people)):
-            if mask == 1:
-                continue
-            elif boat + person <= limit:
-                rescued[i] = 1
-                onBoard[i] = person
-                boat += person
-            else:
-                key_list = list(onBoard.keys())
-                key_list.sort(key=lambda x: onBoard[x])
-                for j in key_list:
-                    if boat < boat - onBoard[j] + person <= limit:
-                        rescued[j] = 0
-                        boat -= onBoard[j]
-                        del onBoard[j]
-
-                        rescued[i] = 1
-                        onBoard[i] = person
-                        boat += person
-                        if boat == limit:
-                            break
-        answer += 1
-    return answer
+# # 40 점  // 최대 두명만 탈수 있음!! 그 조건 빼먹고 풂.
+# def solution(people, limit):
+#     minimum = 40
+#     rescued = [1 if limit - minimum < person else 0 for person in people]
+#     answer = sum(rescued)
+#     while sum(rescued) < len(people):
+#         boat = 0
+#         onBoard = {}
+#         for i, (mask, person) in enumerate(zip(rescued, people)):
+#             if mask == 1:
+#                 continue
+#             elif boat + person <= limit:
+#                 rescued[i] = 1
+#                 onBoard[i] = person
+#                 boat += person
+#             else:
+#                 key_list = list(onBoard.keys())
+#                 key_list.sort(key=lambda x: onBoard[x])
+#                 for j in key_list:
+#                     if boat < boat - onBoard[j] + person <= limit:
+#                         rescued[j] = 0
+#                         boat -= onBoard[j]
+#                         del onBoard[j]
+#
+#                         rescued[i] = 1
+#                         onBoard[i] = person
+#                         boat += person
+#                         if boat == limit:
+#                             break
+#         answer += 1
+#     return answer
 
 
 # 고쳐보는 중
@@ -59,6 +59,39 @@ def solution(people, limit):
 #     #
 #     # for i in range(len(left)-1):
 #     #     for j in range(i+1,len(left)):
+
+# 50점
+def solution(people, limit):
+    people.sort(reverse=True)
+    minimum = 40
+    rescued = [1 if limit - minimum < person else 0 for person in people]
+    answer = sum(rescued)
+
+    if answer >= len(rescued):
+        return answer
+    else:
+        while sum(rescued) < len(rescued):
+            i = rescued.index(0)
+            rescued.reverse()
+            j = len(rescued) - 1 - rescued.index(0)
+            rescued.reverse()
+            if i == len(rescued) - 1 or (i != j and (people[i] + people[j]) > limit):
+                rescued[i] = 1
+                answer += 1
+            else:
+                onBoardAlone = True
+                for k in range(i + 1, j + 1):
+                    if rescued[k] != 1 and people[i] + people[k] <= limit:
+                        rescued[i] = 1
+                        rescued[k] = 1
+                        answer += 1
+                        onBoardAlone = False
+                        break
+                if onBoardAlone:
+                    rescued[i] = 1
+                    answer = 1
+
+    return answer
 
 
 
