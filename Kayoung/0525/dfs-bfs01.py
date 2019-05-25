6e02d59dc6dddf72cdf213bb7a3767fab234900c
Kayoung <elain202@gmail.com>
2019-05-25 13:48:55 +0900

    Solve dfs-bfs01

--- Kayoung/0525/dfs-bfs01.py
+++ Kayoung/0525/dfs-bfs01.py
@@ -6,28 +6,15 @@ def solution(numbers, target):
     dfs(numbers, 0, target)
     return cnt
 
-def sum(numbers):
-    sum = 0
-    for i in numbers:
-        sum += i
-    return sum
-
 def dfs(numbers, cur, target):
     global cnt
-    if cur == len(numbers):
+    if cur == len(numbers) :
+        if sum(numbers) == target:
+            cnt += 1
         return
 
     dfs(numbers, cur + 1, target)
-    if sum(numbers) == target:
-        cnt += 1
-        return
-
-    numbers[cur] *= -1
+    numbers[cur] *= (-1)
     dfs(numbers, cur + 1, target)
-    if sum(numbers) == target:
-        cnt += 1
-        return
-
-
 
 print( solution([1, 1, 1, 1, 1], 3) )
