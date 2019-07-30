657f72c68d93b527b460946c39dce7ca7cfbac3d
hongdaeyong <ejddjfl67@gmail.com>
2019-07-30 17:12:53 +0900

    git ignore commit

--- DaeYong/10graph/graphProb1_.py
+++ DaeYong/10graph/graphProb1_.py
@@ -1,39 +1,25 @@
-import numpy as np
+class node:
+    def __init__(self, nodeNum):
+        self.num = nodeNum
+        self.connected = []
+        self.distFrom1 = 0 if nodeNum == 1 else -1
 
-
-def dfs(grph, vis, i, j):
-    vis[i] = 1
-    if i == j:
-        return 0
-    else:
-        for k in range(len(vis)):
-            if grph[i][k] and vis[k] == 0:
-                return 1 + dfs(grph, vis, k, j)
-        return 0
+    def connect(self, edge):
+        if edge[0] == self.num:
+            self.connected.append(edge[1])
+        elif edge[1] == self.num:
+            self.connected.append(edge[0])
 
 
 def solution(n, edge):
     answer = 0
-    graph = [[0 for _ in range(n)] for _ in range(n)]
+    graph = [node(i+1) for i in range(n)]
     for e in edge:
-        graph[e[0]-1][e[1]-1] = 1
-        graph[e[1]-1][e[0]-1] = 1
+        for nd in graph:
+            nd.connect(e)
 
-    maxDistance = -1
-    for node in range(n):
-        visit = [0 for _ in range(n)]
-        distance = dfs(graph, visit, 0, node)
-        if distance > maxDistance:
-            maxDistance = distance
+    
 
-    graph = np.array(graph)
-    wayGraph = np.ones(graph.shape)
-    for _ in range(maxDistance):
-        wayGraph = np.matmul(wayGraph, graph)
-
-    for i in wayGraph[0]:
-        if i != 0:
-            answer += 1
     return answer
 
 
