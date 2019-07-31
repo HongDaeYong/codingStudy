89c4d008c63b3d184b4e2dd3ffdaa867aec680b6
hongdaeyong <ejddjfl67@gmail.com>
2019-07-31 17:29:55 +0900

    4 무지의 먹방 라이브 정확성만 품

--- DaeYong/kakao2018/4무지의먹방라이브.py
+++ DaeYong/kakao2018/4무지의먹방라이브.py
@@ -2,15 +2,17 @@ def solution(food_times, k):
     if sum(food_times) <= k:
         return -1
     food_idx = 0
-    for i in range(k):
+    for _ in range(k):
         while food_times[food_idx] == 0:
             food_idx += 1
-            food_idx = food_idx % len(food_times)
+            food_idx = 0 if food_idx == len(food_times) else food_idx
         food_times[food_idx] -= 1
         food_idx += 1
-        food_idx = food_idx % len(food_times)
+        food_idx = 0 if food_idx == len(food_times) else food_idx
+    while food_times[food_idx] == 0:
+        food_idx += 1
+        food_idx = 0 if food_idx == len(food_times) else food_idx
     return food_idx + 1
-
 
 
 food_times = [3, 1, 2]
