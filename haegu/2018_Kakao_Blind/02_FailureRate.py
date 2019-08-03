8c1828b7f1cf5b35e8ee2d2b6dce23e4b2531b9c
hyegu <hyegu0119@gmail.com>
2019-08-03 14:30:24 +0900

    제로디비전에러 수정

--- haegu/2018_Kakao_Blind/02_FailureRate.py
+++ haegu/2018_Kakao_Blind/02_FailureRate.py
@@ -1,15 +1,19 @@
-# 실패율 63점
+# 실패율 81점
 import operator
 import heapq
 def solution(N, stages):
     answer = []
-    dic={}
-    for i in range(1,N+1):
-        a = len(list(filter(lambda x : x>=i, stages)))
-        b = len(list(filter(lambda x : x==i, stages)))
-        dic[i]= b/a
-    answer = heapq.nlargest(len(stages),dic,key=dic.get)  #63점
-    # sorted_dic = sorted(dic.items(), key=operator.i temgetter(1),reverse=True) #63점
+    dic = {}
+    for i in range(1, N + 1):
+        a = len(list(filter(lambda x: x >= i, stages)))  # 스테이지에 도달한 플레이어 수
+        b = len(list(filter(lambda x: x == i, stages)))  # 스테이지에 도달했지만 아직 클리어하지 못한 수
+        if a == 0:
+            dic[i] = 0
+        else:
+            dic[i] = b / a
+
+    answer = heapq.nlargest(len(stages), dic, key=dic.get)  # 81점
+    # sorted_dic = sorted(dic.items(), key=operator.i temgetter(1),reverse=True) #81점
     # for i in sorted_dic:
     #     answer.append(i[0])
     return answer
