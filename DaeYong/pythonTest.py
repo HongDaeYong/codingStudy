661d81e615f076a8ccf0c2189a735c713616a653
hongdaeyong <ejddjfl67@gmail.com>
2019-08-06 21:44:13 +0900

    6 매칭점수 40점

--- DaeYong/pythonTest.py
+++ DaeYong/pythonTest.py
@@ -5,5 +5,9 @@
 # a = a.replace('1', '', 1)
 # print(a)
 
-if '3' < '2':
-    print(True)
+# if '3' < '2':
+#     print(True)
+
+a = "con%    muzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>"
+# print(a.lower().count("Muzi".lower()))
+print(a.find("abcd"))
